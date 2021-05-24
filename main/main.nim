f1354db03431533f735755c6dd9825310f3ba820
Jaremy Creechley <creechley@gmail.com>
2021-05-24 00:42:14 -0700

    adding in ads1115 readings and config setup

--- main/main.nim
+++ main/main.nim
@@ -1,4 +1,5 @@
 import nesper
+import nesper/timers
 import nesper/net_utils
 import nesper/nvs_utils
 import nesper/events
@@ -16,7 +17,12 @@ else:
 # const CONFIG_EXAMPLE_WIFI_SSID = getEnv("WIFI_SSID")
 # const CONFIG_EXAMPLE_WIFI_PASSWORD = getEnv("WIFI_PASSWORD")
 
-const TAG*: cstring = "main"
+const
+  TAG*: cstring = "main"
+  APP_DONE* = EventBits_t(BIT(1))
+
+
+var app_events: EventGroupHandle_t
 
 app_main():
 
@@ -24,20 +30,29 @@ app_main():
 
   # Other startup code
   # ...
+  app_events = xEventGroupCreate()
+
 
   # Connect networking
-  check:
-    networkingConnect()
+  # check:
+    # networkingConnect()
 
   # Connect networking
-  onNetworking():
-    logi(TAG, "Connected to %s", networkConnectionName)
-    logi(TAG, "IPv4 address: %s", $networkIpAddr)
-    logi(TAG, "network setup!\n")
-    run_server()
+  # onNetworking():
+    # logi(TAG, "Connected to %s", networkConnectionName)
+    # logi(TAG, "IPv4 address: %s", $networkIpAddr)
+    # logi(TAG, "network setup!\n")
+
+  run_server()
 
   # turn off networking 
-  check:
-    networkingDisconnect()
+  # check:
+    # networkingDisconnect()
+
+  discard app_events.xEventGroupWaitBits(APP_DONE, 1, 1, portMAX_DELAY)
+
+  while true:
+    logi(TAG, "network ok\n")
+    delay(1000.Millis)
 
   assert false, "shouldn't reach here!"
